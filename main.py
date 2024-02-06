be080f07aa45fc0483629ba66d1c9b3790e309ff
ReDedrik <redmanesh@gmail.com>
2024-02-05 19:53:45 -0600

    added imports

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import pandas as pd
+import tkinter as tk
+import os
+import astropy.units as units
+from astropy.stats import SigmaClip
+from astropy.visualization import simple_norm
+from astropy.convolution import convolve
+from astropy.io import fits
+from astropy.stats import sigma_clipped_stats
 
+
+file = fits.open("C:/Users/redma/Downloads/SPT2147-50-sigmaclipped-g395m-s3d_v2.fits")
